bae2707a237ebb697e5b25c9cfed879224d250eb
zuhayrtariq <zuhayrtariq4@gmail.com>
2023-08-23 23:57:56 +0500

    Completed

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Key': '3c250da754mshf1c8262ddd46628p1e68eejsn2e44abb0ee7c',
+    'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+  },
+};
+
+export const youtubeOptions = {
+  method: 'GET',
+  headers: {
+    'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+    'X-RapidAPI-Key': 'f0021db587msh781fb1cbef39856p11c183jsn45521d5d1c85',
+  },
+};
+
+export const fetchData = async (url, options) => {
+  const res = await fetch(url, options);
+  const data = await res.json();
+
+  return data;
+};
